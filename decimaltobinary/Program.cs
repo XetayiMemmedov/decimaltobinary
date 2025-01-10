300dcef8470443327ef99f681d8314c76a6dca65
XetayiMemmedov <khatayirm@code.edu.az>
2025-01-10 10:21:12 +0400

    Add project files.

---
+++ decimaltobinary/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Bir eded daxil edin");
+int a=int.Parse(Console.ReadLine());
+int qaliq = 0;
+int temp1 = 1;
+int temp = a;
+int binary = 0;
+while (temp > 0)
+{
+    qaliq = temp%2;
+    temp1 = temp1 * 10 + qaliq;
+    temp = temp/2;
+    if (temp == 0)
+    {
+        Console.WriteLine(temp1);
+        while (temp1 > 0)
+        {
+            qaliq = 0;            
+            qaliq = temp1 % 10;
+            binary = (binary + qaliq) *10;
+            temp1 = temp1 / 10;
+         }
+    }
+
+}
+binary = binary / 10;
+Console.WriteLine(binary);
